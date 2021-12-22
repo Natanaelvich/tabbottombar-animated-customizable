b6ab86796b5b9af316b0b3431f45065e404adc56
Natanael Lima <taelima1997@gmail.com>
2021-12-21 23:53:30 -0300

    add routes

---
+++ routes/index.tsx
@@ -0,0 +1,10 @@
+import { NavigationContainer } from '@react-navigation/native';
+import TabBottom from './TabBottom';
+
+export default function Routes() {
+  return (
+    <NavigationContainer >
+      <TabBottom />
+    </NavigationContainer>
+  );
+}
